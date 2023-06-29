0751ace4725546d991524c3a932500b396abc2f2
newb1er <taiwanmichael429@gmail.com>
2023-06-29 22:47:21 +0800

    chore: postcss setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import postcssPresetEnv from "postcss-preset-env"
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  css: {
+    postcss: {
+      plugins: [postcssPresetEnv()]
+    }
+  }
 })
